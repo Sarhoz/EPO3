468d24c64250090698c7d4af0b7ec71843051788
Sarhoz <sozan1@live.nl>
2022-12-06 12:33:31 +0100

    Final of 6-12-2022
    mmm

--- EPO/Counter for fsm.vhd
+++ EPO/Counter for fsm.vhd
@@ -1,19 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity counter is
 	port(	reset: 	in std_logic;
 		Reset_count: in std_logic;
 		clk: in std_logic;
-		count:  out std_logic_vector (19 downto 0)	);
+		count_out:  out std_logic_vector (16 downto 0)	);
 end counter;
 
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 architecture behaviour of counter is
-	signal count, new_count		: unsigned (19 downto 0);
+	signal count, new_count		: unsigned (16 downto 0);
 
+begin
 -- Register process
 	process (clk)
 	begin
@@ -25,3 +28,14 @@ architecture behaviour of counter is
 			end if;
 		end if;
 	end process;
+
+
+	-- Adder process
+	process (count)
+	begin
+		new_count 	<= count + 1;
+	end process;
+
+	count_out	<= std_logic_vector (count);
+
+end architecture behaviour;
--- EPO/Decoder of SOUND.vhd
+++ EPO/Decoder of SOUND.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity decoder is
 	port(	reset: 	in std_logic;
 		sig:	in std_logic;
-		count: in std_logic;
+		count_in: in std_logic_vector (16 downto 0);
 		clk: in std_logic;
 		Reset_count: out std_logic;
 		sync: out std_logic;
@@ -22,15 +22,15 @@ begin
 	lbl1: process (clk)
 	begin
 	if (clk'event and clk = '1') then
-	if res = '1' then
+	if reset = '1' then
 	state <= R;
 	else
 	state <= new_state;
 	end if;
 	end if;
-end process
+end process;
 
-	lb12: process (state, sig, count, clk)
+	lb12: process (state, sig, count_in, clk)
 	begin
 
 	case state is
@@ -49,9 +49,9 @@ when S1 =>
 	Reset_count <= '0';
 	sync <= '0';
 	colour <=  '0';
-	if (sig = '0' and count > '??') then
+	if (sig = '0' and count_in > "11011011101110011") then -- meer dan 112499 tellen
 	new_state <= S2;
-	elsif (sig = '1' and count < '??') then
+	elsif (sig = '0' and count_in < "11011011101110100") then -- minder dan 112500 (gelijk aan 4.5 ms) tellen
 	new_state <= R;
 	else
 	new_state <= S1;
@@ -72,10 +72,10 @@ when S3 =>
 	Reset_count <= '0';
 	sync <= '0';
 	colour <=  '0';
-	if (sig = '0' and count > '??') then
-	new_state <= W;
-	elsif (sig = '0' and count < '??') then
+	if (sig = '0' and count_in > "01100001101001111") then -- meer dan 49999 tellen
 	new_state <= Z;
+	elsif (sig = '0' and count_in < "01100001101010000") then --minder dan 50 000 (gelijk aan 2 ms) tellen
+	new_state <= W;
 	else
 	new_state <= S3;
 	end if;
@@ -85,27 +85,18 @@ when W =>
 	sync <= '1';
 	colour <=  '0';
 	new_state <= SC_W;
-	end if;
 
 when Z =>
 	Reset_count <= '1';
 	sync <= '1';
 	colour <=  '1';
 	new_state <= SC_Z;
-	end if;
-
-when Z =>
-	Reset_count <= '1';
-	sync <= '1';
-	colour <=  '1';
-	new_state <= SC_Z;
-	end if;
 
 when SC_W =>
 	Reset_count <= '0';
 	sync <= '1';
 	colour <=  '0';
-	if (count > '??') then
+	if (count_in > "01100001101001001") then -- meer dan 9 tellen
 	new_state <= S2;
 	else
 	new_state <= SC_W;
@@ -115,11 +106,11 @@ when SC_Z =>
 	Reset_count <= '0';
 	sync <= '1';
 	colour <=  '1';
-	if (count > '??') then
+	if (count_in > "01100001101001001") then -- meer dan 9 tellen
 	new_state <= S2;
 	else
 	new_state <= SC_Z;
-	end if;
+	end if;						-- tellen = clock cycles
 
 	end case;
 	end process;
